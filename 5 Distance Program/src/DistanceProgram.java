8808228fe43453989d77344793b8cab21a1c5519
write55 <writeusernamehere55@gmail.com>
2018-09-13 21:49:00 -0400

    Added empty distance program - test the eclipse project extension

---
+++ 5 Distance Program/src/DistanceProgram.java
@@ -0,0 +1,13 @@
+
+// Aaron Wu
+// 9/13/18
+// Program to randomly generate two (x,y) coordinates then calculate the distance between those two points
+
+public class DistanceProgram {
+
+    public static void main(String[] args) {
+
+        
+
+    }
+}
