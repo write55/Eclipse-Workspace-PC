e5627ba409d0f6e025e3f00e3df1c14804ef8f25
write55 <42893610+write55@users.noreply.github.com>
2018-09-24 10:53:57 -0400

    Add files via upload

---
+++ Fraction.java
@@ -0,0 +1,94 @@
+
+// Aaron Wu
+// 9/24/18
+// Program to add, subtract, multiply, and divide fractions, using methods and objects
+// Objects are used to store fractions and methods are used for each operation
+
+public class Fraction {
+	// Make Variables
+	private int numerator;
+	private int denominator;
+
+	// Constructs a Fraction object
+	// Takes arguments and assigns them to numerator and denominator
+	public Fraction(int num, int den) {
+		numerator = num;
+		denominator = den;
+	}
+
+	// Getter for Numerator
+	public int getNumerator() {
+		return numerator;
+	}
+
+	// Getter for Denominator
+	public int getDenominator() {
+		return denominator;
+	}
+
+	// Setter for Numerator
+	public void setNumerator(int num) {
+		numerator = num;
+	}
+
+	// Setter for Denominator
+	public void setDenominator(int den) {
+		denominator = den;
+	}
+
+	/*
+	 * Adds another fraction object given by the argument to the original
+	 * fraction, uses common denominator to add and outputs a new fraction
+	 * object as the result
+	 */
+	public Fraction add(Fraction other) {
+		int sumnum = (getNumerator() * other.getDenominator()) + (other.getNumerator() * getDenominator());
+		int newden = other.getDenominator() * getDenominator();
+		Fraction answer = new Fraction(sumnum, newden);
+		return answer;
+	}
+
+	public Fraction subtract(Fraction other) {
+		int sumdiff = (getNumerator() * other.getDenominator()) - (other.getNumerator() * getDenominator());
+		int newden = other.getDenominator() * getDenominator();
+		Fraction answer = new Fraction(sumdiff, newden);
+		return answer;
+	}
+
+	public Fraction multiply(Fraction other) {
+		int prodnum = getNumerator() * other.getNumerator();
+		int proddem = getDenominator() * other.getDenominator();
+		Fraction answer = new Fraction(prodnum, proddem);
+		return answer;
+	}
+
+	public Fraction divide(Fraction other) {
+		int prodnum = getNumerator() * other.getDenominator();
+		int proddem = getDenominator() * other.getNumerator();
+		Fraction answer = new Fraction(prodnum, proddem);
+		return answer;
+	}
+
+	// Turns answer into a string for output
+	public String toString() {
+		return getNumerator() + "/" + getDenominator();
+	}
+
+	public static void main(String[] args) {
+
+		Fraction f1 = new Fraction(1, 2);
+		Fraction f2 = new Fraction(3, 5);
+
+		Fraction sum = f1.add(f2);
+		System.out.println(f1.toString() + " + " + f2.toString() + " = " + sum.toString());
+
+		Fraction difference = f1.subtract(f2);
+		System.out.println(f1.toString() + " - " + f2.toString() + " = " + difference.toString());
+
+		Fraction product = f1.multiply(f2);
+		System.out.println(f1.toString() + " * " + f2.toString() + " = " + product.toString());
+
+		Fraction quotient = f1.divide(f2);
+		System.out.println(f1.toString() + " / " + f2.toString() + " = " + quotient.toString());
+	}
+}
