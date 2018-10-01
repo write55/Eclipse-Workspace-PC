ed183932dc63727778ada613d65e514e912d5e01
write55 <42893610+write55@users.noreply.github.com>
2018-10-01 10:56:32 -0400

    Finished Javaland Program

--- 6 Javaland/src/Javaland.java
+++ 6 Javaland/src/Javaland.java
@@ -1,5 +1,10 @@
+
 // Aaron Wu
 // 9/28/18
+// Program taxes input from the user creates a "Javaland" object to store the income. 
+// It then uses if statements in the applytax() method to calculate taxes and then prints out the result.
+
+import java.io.*;
 
 public class Javaland {
 
@@ -29,13 +34,83 @@ public class Javaland {
 	public void setIncome(int i) {
 		income = i;
 	}
-	
+
+	// Calcluates tax from a Javaland object storing income
+	public double applyTax() {
+		double tax = 0.0;
+		if (income > LIVINGLIMIT) {
+			tax += LIVINGFEE;
+		}
+		if (income > LIM1 && income < LIM2) {
+			tax += (income - LIM1) * PERCENT2;
+			tax += LIM1 * PERCENT1;
+		} else if (income > LIM2) {
+			tax += LIM1 * PERCENT1;
+			tax += (LIM2 - LIM1) * PERCENT2;
+			tax += (income - LIM2) * PERCENT3;
+		} else {
+			tax += income * PERCENT1;
+		}
+		return tax;
+	}
+
 	// Changes Javaland object to string for output
 	public String toString() {
 		return "Income is " + getIncome();
 	}
-	
-	public static void main (String[] args) {
-		
+
+	// Gets Input for income and returns an int
+	public int getInput() throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("Enter your current income rounded to the nearest integer: ");
+		String input = in.readLine();
+		System.out.println("You entered " + input + " For your income");
+		income = new Integer(input);
+		return income;
+
+	}
+
+	/*
+	 * Runs getInput() method and assigns int to Javaland object, then
+	 * calculates tax with applyTax() then prints the answer
+	 */
+	public static void main(String[] args) throws IOException {
+		Javaland income = new Javaland(0);
+		income.getInput();
+		double tax = income.applyTax();
+		System.out.println(income.toString());
+		System.out.println("Tax is " + tax);
 	}
 }
+
+// Outputs
+
+// Enter your current income rounded to the nearest integer: 0
+// You entered 0 For your income
+// Income is 0
+// Tax is 0.0
+
+// Enter your current income rounded to the nearest integer: 500
+// You entered 500 For your income
+// Income is 500
+// Tax is 5.0
+
+// Enter your current income rounded to the nearest integer: 5432
+// You entered 5432 For your income
+// Income is 5432
+// Tax is 554.32
+
+// Enter your current income rounded to the nearest integer: 15006
+// You entered 15006 For your income
+// Income is 15006
+// Tax is 850.3
+
+// Enter your current income rounded to the nearest integer: 32999
+// You entered 32999 For your income
+// Income is 32999
+// Tax is 1899.9
+
+// Enter your current income rounded to the nearest integer: 100555
+// You entered 100555 For your income
+// Income is 100555
+// Tax is 8655.5
